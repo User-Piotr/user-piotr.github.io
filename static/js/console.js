dc0fe12fbf53f053aa83225cc07e9c0405001663
User-Piotr <piotr@kmiecik.it>
2026-04-12 13:44:20 +0200

    Hugo: add console

--- static/js/console.js
+++ static/js/console.js
@@ -1,14 +1,14 @@
 (function () {
   var reset  = 'color: inherit; font-size: 13px;';
-  var green  = 'color: #4af626; font-size: 13px;';
-  var bold   = 'color: #4af626; font-weight: bold; font-size: 13px;';
+  var green  = 'color: #ffb000; font-size: 13px;';
+  var bold   = 'color: #ffb000; font-weight: bold; font-size: 13px;';
   var dim    = 'color: #888; font-size: 12px;';
 
-  console.log('%c┌──(piotr㉿showcase)-[~]', green);
+  console.log('%c┌──(piotr@kmiecik.it)', green);
   console.log('%c└─$ whoami', green);
   console.log('%c  DevOps Engineer | SRE', bold);
   console.log(' ');
-  console.log('%c┌──(piotr㉿showcase)-[~]', green);
+  console.log('%c┌──(piotr@kmiecik.it)', green);
   console.log('%c└─$ cat contact.txt', green);
   console.log('%c  ✉  piotr@kmiecik.it', reset);
   console.log('%c  🐙 github.com/User-Piotr', reset);
